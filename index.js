06785090a42a3c2ceac27e07979ca1c17d61cea3
Qbius <mich@lsurowka.email>
2021-07-28 19:55:34 +0200

    ok it's pretty enough now

---
+++ index.js
@@ -0,0 +1,559 @@
+const survivors = {
+    "Acrid": {
+        "img": "images/acrid.png",
+        "skills": {
+            "Misc": [
+                {
+                    "name": "Poison",
+                    "img": "images/poison.png"
+                },
+                {
+                    "name": "Blight",
+                    "img": "images/blight.png"
+                }
+            ],
+            "Primary": [
+                {
+                    "name": "Vicious Wounds",
+                    "img": "images/vicious_wounds.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Neurotoxin",
+                    "img": "images/neurotoxin.png"
+                },
+                {
+                    "name": "Ravenous Bite",
+                    "img": "images/ravenous_bite.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Caustic Leap",
+                    "img": "images/caustic_leap.png"
+                },
+                {
+                    "name": "Frenzied Leap",
+                    "img": "images/frenzied_leap.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "Epidemic",
+                    "img": "images/epidemic.png"
+                }
+            ]
+        }
+    },
+    "Artificer": {
+        "img": "images/artificer.png",
+        "skills": {
+            "Passive": [
+                {
+                    "name": "ENV Suit",
+                    "img": "images/env_suit.png"
+                }
+            ],
+            "Primary": [
+                {
+                    "name": "Flame Bolt",
+                    "img": "images/flame_bolt.png"
+                },
+                {
+                    "name": "Plasma Bolt",
+                    "img": "images/plasma_bolt.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Charged Nano-Bomb",
+                    "img": "images/charged_nano-bomb.png"
+                },
+                {
+                    "name": "Cast Nano-Spear",
+                    "img": "images/cast_nano-spear.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Snapfreeze",
+                    "img": "images/snapfreeze.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "Flamethrower",
+                    "img": "images/flamethrower.png"
+                },
+                {
+                    "name": "Ion Surge",
+                    "img": "images/ion_surge.png"
+                }
+            ]
+        }
+    },
+    "Bandit": {
+        "img": "images/bandit.png",
+        "skills": {
+            "Passive": [
+                {
+                    "name": "Backstab",
+                    "img": "images/backstab.png"
+                }
+            ],
+            "Primary": [
+                {
+                    "name": "Burst",
+                    "img": "images/burst.png"
+                },
+                {
+                    "name": "Blast",
+                    "img": "images/blast.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Serrated Dagger",
+                    "img": "images/serrated_dagger.png"
+                },
+                {
+                    "name": "Serrated Shiv",
+                    "img": "images/serrated_shiv.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Smoke Bomb",
+                    "img": "images/smoke_bomb.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "Lights Out",
+                    "img": "images/lights_out.png"
+                },
+                {
+                    "name": "Desperado",
+                    "img": "images/desperado.png"
+                }
+            ]
+        }
+    },
+    "Captain": {
+        "img": "images/captain.png",
+        "skills": {
+            "Passive": [
+                {
+                    "name": "Defensive Microbots",
+                    "img": "images/defensive_microbots.png"
+                }
+            ],
+            "Primary": [
+                {
+                    "name": "Vulcan Shotgun",
+                    "img": "images/vulcan_shotgun.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Power Tazer",
+                    "img": "images/power_tazer.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Orbital Probe",
+                    "img": "images/orbital_probe.png"
+                },
+                {
+                    "name": "OGM-72 'DIABLO' Strike",
+                    "img": "images/ogm-72_diablo_strike.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "Orbital Supply Beacon",
+                    "img": "images/orbital_supply_beacon.png"
+                }
+            ],
+            "Misc1": [
+                {
+                    "name": "Beacon: Healing",
+                    "img": "images/beacon:_healing.png"
+                },
+                {
+                    "name": "Beacon: Shocking",
+                    "img": "images/beacon:_shocking.png"
+                },
+                {
+                    "name": "Beacon: Resupply",
+                    "img": "images/beacon:_resupply.png"
+                },
+                {
+                    "name": "Beacon: Hacking",
+                    "img": "images/beacon:_hacking.png"
+                }
+            ],
+            "Misc2": [
+                {
+                    "name": "Beacon: Healing",
+                    "img": "images/beacon:_healing.png"
+                },
+                {
+                    "name": "Beacon: Shocking",
+                    "img": "images/beacon:_shocking.png"
+                },
+                {
+                    "name": "Beacon: Resupply",
+                    "img": "images/beacon:_resupply.png"
+                },
+                {
+                    "name": "Beacon: Hacking",
+                    "img": "images/beacon:_hacking.png"
+                }
+            ]
+        }
+    },
+    "Commando": {
+        "img": "images/commando.png",
+        "skills": {
+            "Primary": [
+                {
+                    "name": "Double Tap",
+                    "img": "images/double_tap.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Phase Round",
+                    "img": "images/phase_round.png"
+                },
+                {
+                    "name": "Phase Blast",
+                    "img": "images/phase_blast.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Tactical Dive",
+                    "img": "images/tactical_dive.png"
+                },
+                {
+                    "name": "Tactical Slide",
+                    "img": "images/tactical_slide.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "Suppressive Fire",
+                    "img": "images/suppressive_fire.png"
+                },
+                {
+                    "name": "Frag Grenade",
+                    "img": "images/frag_grenade.png"
+                }
+            ]
+        }
+    },
+    "Engineer": {
+        "img": "images/engineer.png",
+        "skills": {
+            "Primary": [
+                {
+                    "name": "Bouncing Grenades",
+                    "img": "images/bouncing_grenades.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Pressure Mines",
+                    "img": "images/pressure_mines.png"
+                },
+                {
+                    "name": "Spider Mines",
+                    "img": "images/spider_mines.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Bubble Shield",
+                    "img": "images/bubble_shield.png"
+                },
+                {
+                    "name": "Thermal Harpoons",
+                    "img": "images/thermal_harpoons.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "TR12 Gauss Auto-Turret",
+                    "img": "images/tr12_gauss_auto-turret.png"
+                },
+                {
+                    "name": "TR58 Carbonizer Turret",
+                    "img": "images/tr58_carbonizer_turret.png"
+                }
+            ]
+        }
+    },
+    "Huntress": {
+        "img": "images/huntress.png",
+        "skills": {
+            "Primary": [
+                {
+                    "name": "Strafe",
+                    "img": "images/strafe.png"
+                },
+                {
+                    "name": "Flurry",
+                    "img": "images/flurry.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Laser Glaive",
+                    "img": "images/laser_glaive.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Blink",
+                    "img": "images/blink.png"
+                },
+                {
+                    "name": "Phase Blink",
+                    "img": "images/phase_blink.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "Arrow Rain",
+                    "img": "images/arrow_rain.png"
+                },
+                {
+                    "name": "Ballista",
+                    "img": "images/ballista.png"
+                }
+            ]
+        }
+    },
+    "Loader": {
+        "img": "images/loader.png",
+        "skills": {
+            "Passive": [
+                {
+                    "name": "Scrap Barrier",
+                    "img": "images/scrap_barrier.png"
+                }
+            ],
+            "Primary": [
+                {
+                    "name": "Knuckleboom",
+                    "img": "images/knuckleboom.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Grapple Fist",
+                    "img": "images/grapple_fist.png"
+                },
+                {
+                    "name": "Spiked Fist",
+                    "img": "images/spiked_fist.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Charged Gauntlet",
+                    "img": "images/charged_gauntlet.png"
+                },
+                {
+                    "name": "Thunder Gauntlet",
+                    "img": "images/thunder_gauntlet.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "M551 Pylon",
+                    "img": "images/m551_pylon.png"
+                },
+                {
+                    "name": "Thunderslam",
+                    "img": "images/thunderslam.png"
+                }
+            ]
+        }
+    },
+    "Mercenary": {
+        "img": "images/mercenary.png",
+        "skills": {
+            "Passive": [
+                {
+                    "name": "Cybernetic Enhancements",
+                    "img": "images/cybernetic_enhancements.png"
+                }
+            ],
+            "Primary": [
+                {
+                    "name": "Laser Sword",
+                    "img": "images/laser_sword.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Whirlwind",
+                    "img": "images/whirlwind.png"
+                },
+                {
+                    "name": "Rising Thunder",
+                    "img": "images/rising_thunder.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Blinding Assault",
+                    "img": "images/blinding_assault.png"
+                },
+                {
+                    "name": "Focused Assault",
+                    "img": "images/focused_assault.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "Eviscerate",
+                    "img": "images/eviscerate.png"
+                },
+                {
+                    "name": "Slicing Winds",
+                    "img": "images/slicing_winds.png"
+                }
+            ]
+        }
+    },
+    "MUL-T": {
+        "img": "images/mul-t.png",
+        "skills": {
+            "Primary": [
+                {
+                    "name": "Auto-Nailgun",
+                    "img": "images/auto-nailgun.png"
+                },
+                {
+                    "name": "Rebar Puncher",
+                    "img": "images/rebar_puncher.png"
+                },
+                {
+                    "name": "Scrap Launcher",
+                    "img": "images/scrap_launcher.png"
+                },
+                {
+                    "name": "Power-Saw",
+                    "img": "images/power-saw.png"
+                }
+            ],
+            "Misc": [
+                {
+                    "name": "Auto-Nailgun",
+                    "img": "images/auto-nailgun.png"
+                },
+                {
+                    "name": "Rebar Puncher",
+                    "img": "images/rebar_puncher.png"
+                },
+                {
+                    "name": "Scrap Launcher",
+                    "img": "images/scrap_launcher.png"
+                },
+                {
+                    "name": "Power-Saw",
+                    "img": "images/power-saw.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "Blast Canister",
+                    "img": "images/blast_canister.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "Transport Mode",
+                    "img": "images/transport_mode.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "Retool",
+                    "img": "images/retool.png"
+                },
+                {
+                    "name": "Power Mode",
+                    "img": "images/power_mode.png"
+                }
+            ]
+        }
+    },
+    "REX": {
+        "img": "images/rex.png",
+        "skills": {
+            "Passive": [
+                {
+                    "name": "Natural Toxins",
+                    "img": "images/natural_toxins.png"
+                }
+            ],
+            "Primary": [
+                {
+                    "name": "DIRECTIVE: Inject",
+                    "img": "images/directive:_inject.png"
+                }
+            ],
+            "Secondary": [
+                {
+                    "name": "DIRECTIVE: Drill",
+                    "img": "images/directive:_drill.png"
+                },
+                {
+                    "name": "Seed Barrage",
+                    "img": "images/seed_barrage.png"
+                }
+            ],
+            "Utility": [
+                {
+                    "name": "DIRECTIVE: Disperse",
+                    "img": "images/directive:_disperse.png"
+                },
+                {
+                    "name": "Bramble Volley",
+                    "img": "images/bramble_volley.png"
+                }
+            ],
+            "Special": [
+                {
+                    "name": "DIRECTIVE: Harvest",
+                    "img": "images/directive:_harvest.png"
+                },
+                {
+                    "name": "Tangling Growth",
+                    "img": "images/tangling_growth.png"
+                }
+            ]
+        }
+    }
+}
+
+const random_survivor = Object.entries(survivors)[Math.floor(Math.random() * Object.entries(survivors).length)];
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        slot: '',
+        choices: '',
+        skill: '',
+
+        chosen_survivor: random_survivor,
+        chosen_skills: Object.fromEntries(Object.entries(random_survivor[1].skills).map(([slot, choices]) => [slot, Math.floor(Math.random() * choices.length)]))
+    },
+});
